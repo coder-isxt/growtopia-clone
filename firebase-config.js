ec111f570a36bf77fcb25e7e8ccd8012be549f8e
isxt <104578861+coder-isxt@users.noreply.github.com>
2026-02-20 22:18:17 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// 1) Create a Firebase project and Realtime Database (start in test mode for local dev).
+// 2) Open Project settings > General > Your apps > Web app config.
+// 3) Paste your values below.
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAXoXI2ELlKTgmOqFlrm1U2qC3PS-UqLPI",
+  authDomain: "growtopia-clone.firebaseapp.com",
+  databaseURL: "https://growtopia-clone-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "growtopia-clone",
+  storageBucket: "growtopia-clone.firebasestorage.app",
+  messagingSenderId: "39896773497",
+  appId: "1:39896773497:web:d4e5878d403dca175775a8"
+};
