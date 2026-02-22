c1b2139c970cfed8e6d2f089f91c4e0e74022160
isxt <104578861+coder-isxt@users.noreply.github.com>
2026-02-23 00:51:40 +0200

    Add files via upload

--- clothes/pants.js
+++ clothes/pants.js
@@ -1,4 +1,5 @@
 window.GTCosmeticCatalog = window.GTCosmeticCatalog || {};
 window.GTCosmeticCatalog.pants = [
-  { id: "cloth_pants", name: "Cloth Pants", color: "#7e92a3", icon: "PT", faIcon: "fa-solid fa-user", rarity: "common", image: "pants/cloth_pants.png" }
+  { id: "cloth_pants", name: "Cloth Pants", color: "#7e92a3", icon: "PT", faIcon: "fa-solid fa-user", rarity: "common", image: "pants/cloth_pants.png" },
+  { id: "test", name: "test Pants", color: "#7e92a3", icon: "PT", faIcon: "fa-solid fa-user", rarity: "common", image: "pants/test.png" },
 ];
