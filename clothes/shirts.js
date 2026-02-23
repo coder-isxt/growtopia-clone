522037d033b3ae9c60c9c4825d76afe0d7957fef
isxt <104578861+coder-isxt@users.noreply.github.com>
2026-02-23 11:47:19 +0200

    Add files via upload

--- clothes/shirts.js
+++ clothes/shirts.js
@@ -4,5 +4,6 @@ window.GTCosmeticCatalog.shirts = [
   { id: "hoodie_blue", name: "Blue Hoodie", color: "#5f8cff", icon: "HD", faIcon: "fa-solid fa-shirt", rarity: "rare", image: "clothes/hoodie_blue.png" },
   { id: "armor_iron", name: "Iron Armor", color: "#a9b5c2", icon: "AR", faIcon: "fa-solid fa-shield-halved", rarity: "epic", image: "clothes/armor_iron.png" },
   { id: "oop_shirt", name: "OOP Shirt", color: "#ffffff", icon: "OOP", faIcon: "fa-solid fa-shield-halved", rarity: "rare", image: "clothes/oop_shirt.png" },
-  { id: "oop_shirt_red", name: "Red OOP Shirt", color: "#ffffff", icon: "ROOP", faIcon: "fa-solid fa-shield-halved", rarity: "epic", image: "clothes/oop_shirt_red.png" }
+  { id: "oop_shirt_red", name: "Red OOP Shirt", color: "#ffffff", icon: "ROOP", faIcon: "fa-solid fa-shield-halved", rarity: "epic", image: "clothes/oop_shirt_red.png" },
+  { id: "sun_shirt", name: "Sun Shirt", color: "#ffffff", icon: "SS", faIcon: "fa-solid fa-shield-halved", rarity: "epic", image: "clothes/sun_shirt.png" }
 ];
