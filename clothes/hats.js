a35a24573dc251f0fd9dbab20ea5f2910ddb8ab5
isxt <104578861+coder-isxt@users.noreply.github.com>
2026-02-23 12:57:21 +0200

    Add files via upload

--- clothes/hats.js
+++ clothes/hats.js
@@ -4,5 +4,5 @@ window.GTCosmeticCatalog.hats = [
   { id: "basic_cap", name: "Basic Cap", color: "#d7c7a3", icon: "HT", faIcon: "fa-solid fa-hat-cowboy-side", rarity: "common", image: "hats/la.png" },
   { id: "silinter", name: "Silinter", color: "#d7c7a3", icon: "HT", faIcon: "fa-solid fa-hat-cowboy-side", rarity: "common", image: "hats/silinter.png" },
   { id: "chinese", name: "Chinese", color: "#0e0d0c", icon: "CH", faIcon: "fa-solid fa-hat-cowboy-side", rarity: "rare", image: "hats/chinese.png" },
-
+{ id: "flatcap", name: "Flatcap", color: "#0e0d0c", icon: "FC", faIcon: "fa-solid fa-hat-cowboy-side", rarity: "rare", image: "hats/flatcap.png" },
 ];
