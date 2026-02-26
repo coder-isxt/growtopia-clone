dd122efd8d175d194aec02801ba641a0037b4f0e
isxt <104578861+coder-isxt@users.noreply.github.com>
2026-02-26 16:41:20 +0200

    Delete clothes directory

--- clothes/swords.js
+++
@@ -1,8 +0,0 @@
-window.GTCosmeticCatalog = window.GTCosmeticCatalog || {};
-window.GTCosmeticCatalog.swords = [
-  { id: "wood_blade", name: "Wood Blade", color: "#9a6a3f", icon: "WB", faIcon: "fa-solid fa-sword", rarity: "common", image: "swords/wood_blade.png", breakMultiplier: 1.05 },
-  { id: "iron_sword", name: "Iron Sword", color: "#c7d2dc", icon: "IS", faIcon: "fa-solid fa-sword", rarity: "rare", image: "swords/iron_sword.png", breakMultiplier: 1.2 },
-  { id: "flame_saber", name: "Flame Saber", color: "#ff7e57", icon: "FS", faIcon: "fa-solid fa-wand-sparkles", rarity: "epic", image: "swords/flame_saber.png", breakMultiplier: 1.35 },
-  { id: "blue_dagger", name: "Blue Dagger", color: "#ffffff", icon: "BD", faIcon: "fa-solid fa-wand-sparkles", rarity: "epic", image: "swords/dagger.png", breakMultiplier: 1.15 },
-  { id: "admin_pickaxe", name: "Admin Pickaxe", color: "#9be7ff", icon: "AP", faIcon: "fa-solid fa-hammer", rarity: "mythic", image: "swords/admin_pickaxe.png", breakMultiplier: 999, instantBreak: true }
-];
